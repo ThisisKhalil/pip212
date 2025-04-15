9476aa2402bdc99f6a11dcbb564836de5e9bdd97
ThisisKhalil <zougar5alil@gmail.com>
2025-04-15 15:24:22 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+# simulateur_financier - app.py
+# Auteur: Zoobir
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    print("Bienvenue dans le simulateur financier 💸")
+    # Ici tu peux coller ton code complet que tu avais dans Colab
+
+if __name__ == "__main__":
+    main()
